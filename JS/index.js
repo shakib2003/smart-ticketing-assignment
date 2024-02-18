c26b040ea82ceb70645c0084ae20de43f4a0d153
shakib2003 <shakib2003skb@gmail.com>
2024-02-19 01:16:15 +0600

    seat and seat price get by js-dom and set in select seat section

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+const seats = document.querySelectorAll('.sit');
+// console.log(seats);
+
+for (let index = 0; index < seats.length; index++) {
+  const seat = seats[index];
+  // console.log(element);
+  seat.addEventListener('click', function(){
+    const seatName = seat.innerText;
+    const seatSelect = document.getElementById('seat-select');
+    const p = document.createElement('p');
+    p.innerText = seatName;
+    seatSelect.appendChild(p);
+    const fare = parseFloat(document.getElementById('fare').innerText);
+    const price = document.getElementById('price');
+    const everySitPrice = document.createElement('p');
+    everySitPrice.innerText = fare;
+    price.appendChild(everySitPrice);
+  })
+}
